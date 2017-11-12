e969e4eface156d469ec9dbcf037ec27c1187142
Sebastian Kalicki <sebastian@kalicki.email>
2017-11-12 18:34:04 +0100

    Add new plugins to core.

--- js/base.js
+++ js/base.js
@@ -1,8 +1,12 @@
 import { BaseCore } from './base.core';
 import { Dropdown } from './plugins/base.dropdown';
+import { DropdownMenu } from './plugins/base.dropdown-menu';
+import { Toggler } from './plugins/base.toggler';
 
-const Base = new BaseCore();
+const Base = window.Base = new BaseCore();
 
-Base.plugin(Dropdown, 'Dropdown');
+Base.plugin(Dropdown);
+Base.plugin(DropdownMenu);
+Base.plugin(Toggler);
 
 Base.init();
