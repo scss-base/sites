d748d902c3b54a1765ed9b10ea456cdbcb59efd5
Sebastian Kalicki <sebastian@kalicki.email>
2017-11-12 18:31:26 +0100

    Add ready method to check whether DOM is already loaded.

--- js/base.core.js
+++ js/base.core.js
@@ -17,6 +17,18 @@ class BaseCore {
     this.plugins = Object.assign({}, this.plugins, {[name]: pluginClass});
   }
 
+  ready(fn) {
+    if (document.readyState != 'loading'){
+      fn();
+    } else if (document.addEventListener) {
+      document.addEventListener('DOMContentLoaded', fn);
+    } else {
+      document.attachEvent('onreadystatechange', function() {
+        if (document.readyState != 'loading')
+          fn();
+      });
+    }
+  }
 }
 
 export {BaseCore};
