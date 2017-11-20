05e15c4a283d684b1f459ccc131794a7663d740d
Sebastian Kalicki <sebastian@kalicki.email>
2017-11-20 23:10:52 +0100

    Add uglifyjs-webpack-plugin

--- gulp/tasks/javascript.js
+++ gulp/tasks/javascript.js
@@ -1,7 +1,9 @@
 const gulp = require('gulp');
+const sequence = require('run-sequence');
 const named = require('vinyl-named');
 const webpack = require('webpack');
 const webpackStream = require('webpack-stream');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const webpackGulpConfig = {
   externals: {},
@@ -19,10 +21,24 @@ const webpackGulpConfig = {
   },
 };
 
+gulp.task('javascript', (cb) => {
+  sequence('javascript:dev', cb);
+});
 
-gulp.task('javascript', function () {
+gulp.task('javascript:dev', function () {
   return gulp.src('js/base.js')
     .pipe(named())
     .pipe(webpackStream(webpackGulpConfig, webpack))
+    .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('javascript:build', function () {
+  return gulp.src('js/base.js')
+    .pipe(named())
+    .pipe(webpackStream(Object.assign({}, webpackGulpConfig, {
+      plugins: [
+        new UglifyJsPlugin()
+      ]
+    }), webpack))
     .pipe(gulp.dest('build/js'));
 });
