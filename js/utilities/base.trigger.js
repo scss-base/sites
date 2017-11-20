1aa0f8d870105508515de9fa668a19862efe8169
Sebastian Kalicki <sebastian@kalicki.email>
2017-11-20 22:17:02 +0100

    Add first example for our javascript library.

--- js/utilities/base.trigger.js
+++ js/utilities/base.trigger.js
@@ -1,99 +1,47 @@
 import { $, $$ } from './base.dom';
-import Plugins from '../plugins';
+import Plugin from '../plugins';
 
-const triggerTypes = ['close', 'open', 'toggle'];
+const triggerTypes = [
+  { name: 'close', plugin: 'Modal' },
+  { name: 'open', plugin: 'Modal' },
+  { name: 'toggle', plugin: 'Toggler' }
+];
+const triggerAttributes = triggerTypes.map(value => `[data-${value.name}]`);
 
 class Trigger {
 
   constructor() {
-
-
-
     this.bindClickEvents();
   }
 
   bindClickEvents() {
-    $$('[data-close], [data-open], [data-toggle]').forEach(element => {
-      const triggerType = this.getTriggerType(element);
+    $$(triggerAttributes).forEach(element => {
+      const triggers = this.getClickTriggers(element);
 
-      console.log(element);
-
-
-      if (triggerType) {
-        element.addEventListener('click', () => {
-          const id = element.dataset[triggerType];
-          const targetElement = $(`#${id}`);
-
-          console.log(Plugins);
-          console.log(targetElement);
-          console.log(element.dataset[triggerType]);
-          console.log('test', element.dataset, triggerType);
-
+      triggers.forEach(trigger => {
+        trigger.element.addEventListener('click', event => {
+          event.preventDefault();
+          triggerTypes.forEach(triggerType => {
+            if (triggerType.name === trigger.type) {
+              const plugin = new Plugin[triggerType.plugin](trigger.targetElement);
+              plugin[trigger.type]();
+            }
+          });
         });
-      }
+      });
     });
   }
 
-  getTriggerType(element) {
-    let type = null;
-
-    Object.keys(element.dataset).forEach(dataName => {
-      switch (dataName) {
-        case 'close':
-          type = 'close';
-          break;
-        case 'open':
-          type = 'open';
-          break;
-        case 'toggle':
-          type = 'toggle';
-          break;
-        default:
-          return false;
-      }
-    });
-
-    return type;
+  /**
+   *
+   * @param element
+   * @returns {{type: string, element: HTMLElement, targetElement: HTMLElement}[]}
+   */
+  getClickTriggers(element) {
+    return Object.keys(element.dataset)
+      .filter(filterValue => triggerTypes.filter(triggerType => triggerType.name === filterValue).length > 0)
+      .map(type => ({ type, element, targetElement: $(`#${element.dataset[type]}`) }));
   }
-
-  getPluginClass() {
-
-  }
-
-  // initCloseListener() {
-  //   $$('[ data-close]').forEach(element => {
-  //     const id = element.dataset.close;
-  //     const selector = $(`#${id}`);
-  //
-  //     if (selector.dataset.hasOwnProperty('modal')) {
-  //       // @todo duplicate
-  //       new Modal(selector);
-  //     }
-  //   });
-  // }
-  //
-  // initOpenListener() {
-  //   $$('[ data-open]').forEach(element => {
-  //     const id = element.dataset.open;
-  //     const selector = $(`#${id}`);
-  //
-  //     if (selector.dataset.hasOwnProperty('modal')) {
-  //       // @todo duplicate
-  //       new Modal(selector);
-  //     }
-  //   });
-  // }
-  //
-  // initToggleListener() {
-  //   $$('[ data-toggle]').forEach(element => {
-  //     element.addEventListener('click', this.toggle);
-  //   });
-  // }
-  //
-  // toggle() {
-  //   const toggler = new Toggler($(`#${this.dataset.toggle}`));
-  //   toggler.toggle();
-  // }
 }
 
 export { Trigger }
