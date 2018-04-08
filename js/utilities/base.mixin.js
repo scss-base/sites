1b1db3f9c0462743db29baaf16a3340cc6418a2b
Sebastian Kalicki <sebastian@kalicki.email>
2018-04-08 17:32:38 +0200

    Add class mixin...

---
+++ js/utilities/base.mixin.js
@@ -0,0 +1,14 @@
+/**
+ * @see http://justinfagnani.com/2015/12/21/real-mixins-with-javascript-classes/
+ */
+class MixinBuilder {
+    constructor(superclass) {
+        this.superclass = superclass;
+    }
+
+    with(...mixins) {
+        return mixins.reduce((c, mixin) => mixin(c), this.superclass);
+    }
+}
+
+export const mix = (superclass) => new MixinBuilder(superclass);
