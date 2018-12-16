020fb01d7dd74752912572ab7acc1cc8558ffcf3
Sebastian Kalicki <sebastian@kalicki.email>
2018-12-16 05:15:38 +0100

    updated watch task

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,6 @@ requireDir('./gulp/tasks');
 
 // Watch files for changes
 gulp.task('watch', function() {
-  gulp.watch(CONFIG.SASS_LINT_FILES, ['sass']);
-  gulp.watch(CONFIG.JS_FILES, ['javascript']);
+  gulp.watch(CONFIG.SASS_LINT_FILES, gulp.series('sass'));
+  gulp.watch(CONFIG.JS_FILES, gulp.series('javascript'));
 });
