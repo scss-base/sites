03196991f9aff5db6107fa730cc8b84673fee345
Sebastian Kalicki <sebastian@kalicki.email>
2018-12-18 03:26:15 +0100

    WIP

--- js/base.js
+++ js/base.js
@@ -3,14 +3,14 @@ import { Dropdown } from './plugins/base.dropdown';
 import { DropdownMenu } from './plugins/base.dropdown-menu';
 import { Toggler } from './plugins/base.toggler';
 import { Trigger } from './utilities/base.trigger';
-import { Emitter } from './utilities/base.emitter';
+// import { Emitter } from './utilities/base.emitter';
 
 const Base = window.Base = new BaseCore();
 Base.plugin(Dropdown);
 Base.plugin(DropdownMenu);
 Base.plugin(Toggler);
 Base.plugin(Trigger);
-Base.plugin(Emitter);
+// Base.plugin(Emitter);
 
 Base.init();
 Base.ready(() => {
@@ -33,5 +33,4 @@ Base.ready(() => {
   // console.log(emitter2);
 
   new Base.Trigger();
-
 });
