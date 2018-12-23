5a873edbe181c7cc7bc6da1a9fdaaecbc4be8015
Sebastian Kalicki <sebastian@kalicki.email>
2018-12-23 06:24:07 +0100

    Added event stuff...

--- src/js/utilities/dom.js
+++ src/js/utilities/dom.js
@@ -1,4 +1,43 @@
 /**
+ * Execute all handlers and behaviors attached to the matched elements for the given event type.
+ *
+ * @param {String} type
+ * @param {*} detail
+ */
+HTMLElement.prototype.emit = function (type, detail = null) {
+  const event = new CustomEvent(type, { detail });
+  this.dispatchEvent(event);
+
+  return this;
+};
+
+/**
+ * Attach an event handler function for one or more events to the selected elements.
+ *
+ * @param {String} type
+ * @param fn
+ * @param options
+ */
+HTMLElement.prototype.on = function (type, fn, options) {
+  this.addEventListener(type, fn, options);
+
+  return this;
+};
+
+/**
+ * Remove an event handler.
+ *
+ * @param type
+ * @param fn
+ * @param options
+ */
+HTMLElement.prototype.off = function (type, fn, options) {
+  this.removeEventListener(type, fn, options);
+
+  return this;
+};
+
+/**
  *
  * @param selectors
  * @param node
