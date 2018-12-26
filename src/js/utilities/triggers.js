ba43d33a79a605cdb8a67e58cf2ffcb7fb1af3fc
Sebastian Kalicki <sebastian@kalicki.email>
2018-12-26 20:35:02 +0100

    Added CloseMe listener, e.g for Modal plugin.

--- src/js/utilities/triggers.js
+++ src/js/utilities/triggers.js
@@ -26,15 +26,48 @@ export default class Triggers {
     $(`#${id}`).trigger('toggle.base.trigger');
   }
 
+  static closeMeListener(event) {
+    const plugin = event.type.split('.')[2];
+    const pluginId = event.detail;
+
+    $$(`[data-${plugin}]`).forEach(element => {
+      if (pluginId !== element.dataset[plugin]) {
+        element.trigger('close.base.trigger');
+      }
+    });
+  }
+
   static addListeners() {
+    Triggers.addOpenListener();
+    Triggers.addCloseListener();
+    Triggers.addToggleListener();
+    Triggers.addCloseMeListener();
+  }
+
+  static addOpenListener() {
     $$('[data-open]')
       .forEach(element =>
-        element.off('click', Triggers.openListener).on('click', Triggers.openListener, false));
+        element.off('click', Triggers.openListener).on('click', Triggers.openListener));
+  }
+
+  static addCloseListener() {
     $$('[data-close]')
       .forEach(element =>
-        element.off('click', Triggers.closeListener).on('click', Triggers.closeListener, false));
+        element.off('click', Triggers.closeListener).on('click', Triggers.closeListener));
+  }
+
+  static addToggleListener() {
     $$('[data-toggle]')
       .forEach(element =>
-        element.off('click', Triggers.toggleListener).on('click', Triggers.toggleListener, false));
+        element.off('click', Triggers.toggleListener).on('click', Triggers.toggleListener));
+  }
+
+  static addCloseMeListener() {
+    const pluginNames = ['dropdown', 'modal', 'tooltip'];
+    pluginNames.forEach(pluginName => {
+      $$(`[data-${pluginName}]`).forEach(element => {
+        element.off(`closeme.base.${pluginName}`, Triggers.closeMeListener).on(`closeme.base.${pluginName}`, Triggers.closeMeListener);
+      });
+    });
   }
 }
