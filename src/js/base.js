21779db8fc4ae51d22b113b7adf762f676d0862e
Sebastian Kalicki <sebastian@kalicki.email>
2018-12-27 15:14:46 +0100

    Clean up...

--- src/js/base.js
+++ src/js/base.js
@@ -1,8 +1,4 @@
-// import { Triggers } from './utilities'
 import { Dropdown, DropdownMenu, Modal, Toggler } from './plugins';
-import { $ } from './utilities';
-
-// Triggers.init();
 
 window.Base = {
   Dropdown,
@@ -10,18 +6,3 @@ window.Base = {
   Modal,
   Toggler,
 };
-
-const $modal1 = $('#modal1');
-//$modal1.on('init.base.modal', () => {
-//  console.log('init.base.modal1');
-//});
-
-const $modal1c = $('#modal1');
-//$modal1c.on('init.base.modal', () => {
-//  console.log('init.base.modal1c');
-//});
-
-
-const modal1 = new Base.Modal($modal1);
-
-//modal1.open();
