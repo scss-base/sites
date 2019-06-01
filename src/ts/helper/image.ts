95108a91d0081f7cd76f30756fcfb8ea9e42ddb5
Sebastian Kalicki <sebastian@kalicki.email>
2019-06-01 02:24:30 +0200

    Added `imageLoader` helper

---
+++ src/ts/helper/image.ts
@@ -0,0 +1,28 @@
+import { one } from './index';
+
+/**
+ * Loader function that helps to trigger a callback when multiple images has been loaded.
+ *
+ * @param {Array} images An array of strings with the paths to the images.
+ * @param {Function} fn Callback function executed when all images has been loaded or not.
+ */
+export function imageLoader(images: NodeListOf<any>, fn: () => void): void {
+  let loaded = 0;
+  const verifier = () => {
+    loaded += 1;
+
+    if (loaded === images.length) {
+      fn.apply(undefined);
+    }
+  };
+
+  images.forEach(image => {
+    if (image.complete) {
+      verifier();
+      return;
+    }
+
+    one('load', image, verifier);
+    one('error', image, verifier);
+  });
+}
