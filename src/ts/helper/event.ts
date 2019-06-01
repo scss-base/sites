b589feb15f46d2f31f1237d3ca4a4c58f64ace9a
Sebastian Kalicki <sebastian@kalicki.email>
2019-06-01 02:23:11 +0200

    Added new function `one()`

--- src/ts/helper/event.ts
+++ src/ts/helper/event.ts
@@ -21,9 +21,28 @@ export function on(
   type: string,
   element: Window | Document | Element,
   listener: EventListener,
-  capture?: boolean,
-  once?: boolean,
-  passive?: boolean
+  capture?: boolean
 ): void {
-  element.addEventListener(type, listener, <AddEventListenerOptions>{ capture, once, passive });
+  element.addEventListener(type, listener, <AddEventListenerOptions>{ capture });
+}
+
+function composeListener(
+  type: string,
+  element: Window | Document | Element,
+  listener: EventListener,
+  capture?: boolean
+): EventListener {
+  return function listenerFn(event: Event) {
+    listener(event);
+    element.removeEventListener(type, listenerFn, <AddEventListenerOptions>{ capture });
+  }
+}
+
+export function one(
+  type: string,
+  element: Window | Document | Element,
+  listener: EventListener,
+  capture?: boolean
+): void {
+  element.addEventListener(type, composeListener(type, element, listener, capture), <AddEventListenerOptions>{ capture });
 }
