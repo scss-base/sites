b30d58bdafa1785192a6ba94dd8f5f40c046b858
Sebastian Kalicki <kalicki@jacob.de>
2019-07-03 16:43:44 +0200

    Bump

--- src/js/plugins/positionable.js
+++
@@ -1,8 +0,0 @@
-import {Plugin} from './plugin';
-
-export class Positionable extends Plugin {
-  init() {
-    this.originalPosition = this.options.get('position');
-    this.originalAlignment = this.options.get('alignment');
-  }
-}
