469adc826257a4e6b1827ef88547a58e7d7ae8eb
Sebastian Kalicki <kalicki@jacob.de>
2019-09-24 12:19:03 +0200

    Clean up

--- src/js_old/helper/index.js
+++
@@ -1,3 +0,0 @@
-export { $, $$ } from './dom';
-export { fire, off, on } from './event';
-export { camelCase, capitalize, kebabCase, pascalCase, random, snakeCase } from './string';
--- src/js_old/utilities/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Core } from './core';
-export { default as Keyboard } from './keyboard';
-export { default as Triggers } from './triggers';
